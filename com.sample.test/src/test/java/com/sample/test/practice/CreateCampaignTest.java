9b6251dc0ddb7ac759c967ab37289b85a3e7561c
Omprakash <Omprakash@Omprakash>
2022-03-23 16:59:23 +0530

    second commit

---
+++ com.sample.test/src/test/java/com/sample/test/practice/CreateCampaignTest.java
@@ -0,0 +1,10 @@
+package com.sample.test.practice;
+
+import org.testng.annotations.Test;
+
+public class CreateCampaignTest {
+@Test
+public void createCampaign() {
+	System.out.println("Campaign created");
+}
+}
